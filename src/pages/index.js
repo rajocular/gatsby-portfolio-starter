716be8e0db9f12c7be1a025e738ea97aa5ff7949
Raj <raja.ms96@gmail.com>
2020-04-21 12:17:34 +0530

    initial setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,72 +1,77 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+import React, { useState, useEffect } from "react"
+import { Grid, Typography, makeStyles } from '@material-ui/core';
+import { StaticQuery, graphql } from 'gatsby';
+import Lottie from 'react-lottie';
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
+import * as animationData from '../../static/loader.json';
+import '../../static/styles.css';
 
-const BlogIndex = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-  const posts = data.allMarkdownRemark.edges
-
-  return (
-    <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
-      <Bio />
-      {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug
-        return (
-          <article key={node.fields.slug}>
-            <header>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-            </header>
-            <section>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </section>
-          </article>
-        )
-      })}
-    </Layout>
-  )
-}
-
-export default BlogIndex
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            description
-          }
-        }
-      }
+const query = graphql`
+  query{
+    sitePage {
+      component
     }
   }
-`
+`;
+
+const useStyles = makeStyles({
+  animationContainer: {
+    height: '100vh',
+    alignItems: 'center',
+    backgroundColor: '#2874A6'
+  },
+});
+
+const HomePage = () => {
+  const classes = useStyles();
+  const [loading, setLoading] = useState(true);
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true, 
+    animationData: animationData.default,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
+  async function onRender() {
+    await setTimeout(() => setLoading(false), 4000);
+  };
+
+  useEffect(() => {
+    onRender();
+  }, []);
+
+  return (
+    <Grid container>
+      {loading
+        ? (
+        <Grid container className={classes.animationContainer}>
+          <Lottie
+            options={defaultOptions}
+            height={300}
+            width={300}
+            isStopped={!loading}
+          />
+        </Grid>
+        )
+        : (
+        <StaticQuery
+          query={query}
+          render={data => {
+              return (
+                <Typography>
+                  Hola! You're in
+                </Typography>
+              );
+            }
+          }
+        />
+        )
+      }
+    </Grid>
+  );
+}
+
+export default HomePage;
